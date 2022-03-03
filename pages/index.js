e846e6eff4d71564d8b4f59fcabced7549aaedd3
pirma <pirmammadov.eldar@gmail.com>
2022-03-03 21:02:11 +0400

    task 03.03.2022/5

--- pages/index.js
+++ pages/index.js
@@ -347,7 +347,7 @@ export default function Home() {
                 </li>
               </ul>
 
-              <span className={"green"}>А это 750 726 рублей в месяц</span>
+              <span className={"green"}>А это 1 286 960 рублей в месяц</span>
 
               <span>Эти траты — плата</span>
               <ul>
